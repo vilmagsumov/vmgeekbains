ae67294393fc8faace753b6c0985e146f0089bee
Vil <vilmagsumov1993@gmail.com>
2020-08-27 17:00:57 +0300

    Bug fix

--- python_base/lesson1/exercise2.py
+++ python_base/lesson1/exercise2.py
@@ -3,11 +3,11 @@
 while True:
     seconds = int(input("Введите число в секундах: "))
     if seconds >= 0:
-        hour = seconds // 3598
-        min = (seconds - (hour * 3598)) // 60
-        sec = seconds - hour * 3598 - min * 60
+        hour = seconds // 3600
+        min = (seconds - (hour * 3600)) // 60
+        sec = seconds - hour * 3600 - min * 60
 
-        print(hour, ":", min, ":", sec)
+        print(hour,":",min,":",sec)
         break
     elif seconds < 0:
         print("Введите правильное число")
